e2f885681702940912aa4f7b5a147f06ef2540e6
KHAIRI AHMAD (DCAZ/SLF-MY) <ahk1shm@bosch.com>
2021-09-26 23:40:32 +0800

    Added comparison operators

---
+++ index.js
@@ -0,0 +1,9 @@
+//making decisions with javascript
+//comparison operators: >, <, >=, <=, ==, !=, === (strict equality), !== (strict non-equality)
+10 > 1; //outputs true
+-10 < 0; //outputs true
+50.5 < 5; //outputs false
+//notice examples above all return a Boolean
+'a' < 'b';  //outputs true because the Unicode value for a lowercase 'a' is 0061 while 'b' is 0062
+'A' > 'a'; //outputs false because the Unicode value for uppercase 'A' is 0041 while 'a' is 0061
+//Be careful when compare strings
